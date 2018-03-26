429d203ec6449ef7cf441431674d2765d7812182
Marco D'Incà <marco.dinca@dataservices.it>
2018-03-26 16:39:01 +0200

    aggiunta homepage e timerpage v0.1

---
+++ src/pages/timer/timer.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { isNumber } from 'ionic-angular/util/util';
+
+@Component({
+  selector: 'page-timer',
+  templateUrl: 'timer.html'
+})
+export class TimerPage {
+  timerLoopTimeout;
+  startTime: Date;
+  elapsedSeconds: number;
+  elapsedMinutes: number;
+  elapsedHours: number;
+  risultati: Array<object>;
+
+  pettorale:string; //Legato al valore del field
+
+  constructor(public navCtrl: NavController) {
+    this.elapsedSeconds = 0;
+    this.elapsedHours = 0;
+    this.elapsedMinutes = 0;
+    this.risultati = [];
+
+    let savedStartTime = localStorage.getItem('timerStartTime');
+    if(savedStartTime !== undefined) {
+        this.startTime = new Date(savedStartTime);
+        this.timerLoop();
+    }
+  }
+
+  startTimer() {
+    this.startTime = new Date();
+    this.timerLoop();
+    localStorage.setItem('timerStartTime', this.startTime.toUTCString());
+  }
+
+  timerLoop() {
+    this.timerLoopTimeout = setTimeout(() => {
+        let stepTime = new Date();
+        let elapsed = Math.floor((stepTime.getTime() - this.startTime.getTime())/1000);
+        this.elapsedHours = Math.floor(elapsed/3600);
+        this.elapsedMinutes = Math.floor(elapsed/60);
+        this.elapsedSeconds = Math.floor(elapsed%60);
+        this.timerLoop();
+    }, 1000);
+  }
+
+  stopTimer() {
+    clearTimeout(this.timerLoopTimeout);
+    localStorage.setItem('timerStartTime', undefined);
+    this.elapsedHours = 0;
+    this.elapsedMinutes = 0;
+    this.elapsedSeconds = 0;
+  }
+
+  onKey(event: KeyboardEvent, pettorale:number) { 
+    if(event.keyCode === 13) { //Se l'evento è un invio
+        this.pettorale = '';
+        this.risultati.push({
+            pettorale: pettorale,
+            ore: this.elapsedHours,
+            minuti: this.elapsedMinutes,
+            secondi: this.elapsedSeconds
+        })
+    }
+  }
+}
